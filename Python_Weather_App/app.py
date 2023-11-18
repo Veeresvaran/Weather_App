643dce4b1bb613ae2ee4f8bb9ca6a115d02509be
Veeresvaran <vveeresvaran@gmail.com>
2023-11-18 09:28:44 +0530

    f_commit

---
+++ Python_Weather_App/app.py
@@ -0,0 +1,11 @@
+import requests
+api_key='d8766776f9332e75eb07f7d07d3d46aa'
+user_input=input("Enter city : ")
+weather_data=requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+if weather_data.json()['cod']=='404':
+    print('No city found')
+else:
+    weather=weather_data.json()["weather"][0]["main"]
+    temp=round(weather_data.json()["main"]["temp"])
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}°F")
